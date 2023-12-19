22acdd88a2c01de8750798c03bd8852bb2387bee
JuanRendon1206 <juanjoseflorezrendon124@gmail.com>
2023-12-19 08:47:53 -0500

    confirmacion inicial

---
+++ js/variablesFacilidad.js
@@ -0,0 +1,14 @@
+let x = 'juan';
+let nombre = 'David';
+let y = 'marin';
+let apellido = 'Quintero';
+let z = 30;
+let edad = 25;
+
+console.log(x);  //nombre
+console.log(y);  //apellido
+console.log(z);  //edad
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
